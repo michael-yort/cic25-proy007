38aca73bf5595714dd15919c81f2621df77a152d
michael <michaelyort@gmail.com>
2025-07-16 19:24:47 +0200

    Merge a remote main

--- src/main/java/es/cic/curso25/proy007/controller/BombillaController.java
+++ src/main/java/es/cic/curso25/proy007/controller/BombillaController.java
@@ -15,31 +15,32 @@ public class BombillaController {
     @Autowired
     private BombillaService service;
 
-    // ✅ Listar todas las bombillas
+    // Listar todas las bombillas
     @GetMapping
     public List<Bombilla> listar() {
         return service.listarBombillas();
     }
 
-    // ✅ Ver una bombilla por ID
+    // Ver una bombilla por ID
     @GetMapping("/{id}")
     public Bombilla verPorId(@PathVariable Long id) {
         return service.buscarPorId(id);
     }
 
-    // ✅ Crear nueva bombilla
+    // Crear nueva bombilla
     @PostMapping
     public long crear(@RequestBody Bombilla bombilla) {
         return service.create(bombilla);
     }
 
-    // ✅ Cambiar estado (toggle)
+    // Encender o apagar la bombilla   SOLO FUNCIONA DESDE LINEA DE COMANDOS curl -X POST http://localhost:8080/bombilla/1/toggle
+
     @PostMapping("/{id}/toggle")
     public Bombilla toggle(@PathVariable Long id) {
         return service.toggle(id);
     }
 
-    // ✅ Eliminar una bombilla
+    // Eliminar una bombilla
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         service.delete(id);
