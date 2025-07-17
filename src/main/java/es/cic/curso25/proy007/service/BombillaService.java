b5b5b58de4e7284b31355109d89b64b832c25a2e
michael <michaelyort@gmail.com>
2025-07-17 19:14:08 +0200

    H2 instalado y configurado

--- src/main/java/es/cic/curso25/proy007/service/BombillaService.java
+++ src/main/java/es/cic/curso25/proy007/service/BombillaService.java
@@ -4,13 +4,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import es.cic.curso25.proy007.model.Bombilla;
-import es.cic.curso25.proy007.repository.BombillaRepository;
+import es.cic.curso25.proy007.repository.BombillaRepositoryImpl;
 
 @Service
 public class BombillaService {
 
     @Autowired
-    private BombillaRepository repo;
+    private BombillaRepositoryImpl repo;
 
     public long create(Bombilla bombilla) {
         return repo.create(bombilla);
